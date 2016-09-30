2bc0752e2915eaccefc8f4a83942ed305bfc9cac
sentient07 <vxrram95@gmail.com>
2016-10-01 01:39:03 +0200

    Randomized signal noise and timing using resource

--- compute_stfts.py
+++ compute_stfts.py
@@ -10,6 +10,7 @@ import pdb
 import itertools
 import theano
 import theano.tensor as T
+import resource
 
 
 def pre_compute_kernels(sample_freq, frequency_range, default_width, output="kernel"):
@@ -33,7 +34,7 @@ def pre_compute_kernels(sample_freq, frequency_range, default_width, output="ker
 
     '''
 
-    if output is not "windows" and "kernel":
+    if output is not "windows" and output is not "kernel":
         raise TypeError("The output has to be either windows / kernel")
 
     kernel = []
@@ -91,18 +92,17 @@ def threeloop_cqt(signal, sample_freq):
     frequency_range = 100.*2.**(1./12.*np.array(np.arange(0, 50)))
     time_range = range(0, int(len(signal) - max_width), int(max_width/2))
     output = np.zeros((len(time_range), len(frequency_range)), dtype=np.complex)
-    time_start = time.time()
+    prior = resource.getrusage(resource.RUSAGE_SELF)
     for m, t in enumerate(time_range):
         for n, f in enumerate(frequency_range):
             current_width = int(1/(.03*f)*sample_freq)
             window = blackman(current_width)
             for i in range(t, t+int(current_width)):
                 output[m, n] += np.exp(i * 1j * (-2 * math.pi) * f / sample_freq) * (signal[i] * window[i-t])
-    time_end = time.time()
-    print("Time taken for execution of CQT is : ")
-    print(time_end - time_start)
+    post = resource.getrusage(resource.RUSAGE_SELF)
+    print("System time: " + str(post.ru_stime - prior.ru_stime))
+    print("User time: " + str(post.ru_utime - prior.ru_utime))
     return output, frequency_range, time_range
-
 
 def cqt_two(signal, s_freq):
     '''
@@ -112,6 +112,7 @@ def cqt_two(signal, s_freq):
     freq = 100.*2.**(1./12.*np.array(range(0, 50)))
     time = range(0, int(len(signal) - max_width), int(max_width/2))
     output = np.zeros((len(time), len(freq)), dtype=np.complex)
+    prior = resource.getrusage(resource.RUSAGE_SELF)
     for l_1, t in enumerate(time):
         for l_2, f in enumerate(freq):
             temp_w = int(1/(.03*f)*s_freq)
@@ -119,6 +120,10 @@ def cqt_two(signal, s_freq):
             w_i = np.arange(t, t+temp_w)
             n_signal = signal[t:t+temp_w]
             output[l_1, l_2] = np.sum((np.exp(w_i * 1j * (-2 * math.pi) * f / s_freq) * windows) * n_signal)
+    
+    post = resource.getrusage(resource.RUSAGE_SELF)
+    print("System time: " + str(post.ru_stime - prior.ru_stime))
+    print("User time: " + str(post.ru_utime - prior.ru_utime))
  
     return output, freq, time
 
@@ -133,47 +138,45 @@ def stft_twoloops(signal, s_freq):
     windows, max_width= pre_compute_kernels(s_freq, freq, signal_width, "windows")
     output = np.zeros((len(time_list), len(freq)))
     i_exp = np.arange(0, max_width)
-    time03 = time.time()
+    prior = resource.getrusage(resource.RUSAGE_SELF)
     for l_1, t in enumerate(time_list):
         # import pdb; pdb.set_trace()
         for l_2, f in enumerate(freq):
             n_signal = signal[t:t+max_width]
             output[l_1, l_2] = np.sum(np.exp(i_exp * 2j * math.pi * f / s_freq) * windows[l_2] * n_signal)
-    time04 = time.time()
-    print("CQT time is :")
-    print(time04 - time03)
+    
+    post = resource.getrusage(resource.RUSAGE_SELF)
+    print("System time: " + str(post.ru_stime - prior.ru_stime))
+    print("User time: " + str(post.ru_utime - prior.ru_utime))
     return output, freq, time_list
         
 # windows[l_2].extend([0] * (len() - (len(my_list))))
 
 
-def cqt_single(signal, width, sample_freq):
+def cqt_single(signal, sample_freq):
     ''' Compute the CQT of a signal with a single loop
     '''
     max_width = 1/(.03*100)*sample_freq
     frequency_range = 100.*2.**(1./12.*np.array(range(0, 50)))
     time_list = range(0, int(len(signal) - max_width), int(max_width/2))
-    output = np.zeros((len(freq), len(time_list)), dtype=np.complex)
-    kernel, k_size = pre_compute_kernels(sample_freq, freq, width)
-    time03 = time.time()
-    n_sig = np.ones((len(time_list) , k_size))
+    output = np.zeros((len(time_list), len(frequency_range)))
+    kernel, max_width = pre_compute_kernels(sample_freq, frequency_range, max_width, "kernel")
+    prior = resource.getrusage(resource.RUSAGE_SELF)
+    n_sig = np.ones((len(time_list) , max_width))
     for l_1, t in enumerate(time_list):
-        try:
-            n_sig[l_1] = signal[l_1:l_1 + k_size]
-        except IndexError as ve:
-                pdb.set_trace()
-    pdb.set_trace()
-    output = n_sig * kernel
-    time04 = time.time()
-    print("Time for CQT computation")
-    print(time04 - time03)
-    return n_sig, freq, time_list
+        output[l_1] = np.sum(signal[l_1:l_1 + max_width] * kernel, axis=1)
+    # output = n_sig.dot(np.asarray(kernel).transpose())
+    post = resource.getrusage(resource.RUSAGE_SELF)
+    print("System time: " + str(post.ru_stime - prior.ru_stime))
+    print("User time: " + str(post.ru_utime - prior.ru_utime))
 
-def theano_stft(signal, width, sample_freq):
+    return output, frequency_range, time_list
 
-    max_width = 1/(.03*100)*sample_freq
-    frequency_range = 100.*2.**(1./12.*np.array(range(0, 50)))
-    time_range = range(0, int(len(signal) - max_width), int(max_width/2))
+
+def theano_stft(signal, width, s_freq):
+    max_width = 1/(.03*100)*s_freq
+    freq = 100.*2.**(1./12.*np.array(range(0, 50)))
+    time = range(0, int(len(signal) - max_width), int(max_width/2))
 
     width_var = T.cscalar() #used as i in the numpy implementation
     freq_var = T.fscalar() #used as f 
@@ -182,13 +185,13 @@ def theano_stft(signal, width, sample_freq):
     stft_out = T.exp(width_var * freq_var) * signal_var
     func = theano.function([width_var, freq_var, signal_var], stft_out, allow_input_downcast=True)
 
-    output = np.zeros((len(frequency_range), len(time_range)), dtype=np.complex)
-    for l_1, t in enumerate(time_range):
-        for l_2, f in enumerate(frequency_range):
+    output = np.zeros((len(freq), len(time)), dtype=np.complex)
+    for l_1, t in enumerate(time):
+        for l_2, f in enumerate(freq):
             width = 1/(.03*f)*s_freq
             for i in range(t, t+int(width)):
 
-                output[l_2, l_1] += func(i * -6.28j, f/sample_freq, signal[i]).tolist()  
+                output[l_2, l_1] += func(i * -6.28j, f/s_freq, signal[i]).tolist()  
 
     return output, freq, time
  
@@ -225,4 +228,11 @@ def vectorized_theano(signal, width, s_freq):
     print(time1 - time0)
     return output, freq, time_list
 
+noise = np.random.randn(132300)
+pure_signal = np.cos(2 * math.pi * 440./44100. * np.arange(132300))
+final_signal = pure_signal + noise
+# out = threeloop_cqt(signal, 44100)
+ground_out = cqt_single(final_signal, 44100)
+pylab.imshow(np.log(np.abs(ground_out[0])), origin='lower', aspect='auto')
+pylab.show()
 
